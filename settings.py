9afa55af885c40cf7f8ce8c69ffb42af4150a4b0
Roy Gero II <roy.gero.ii@gmail.com>
2016-12-05 13:10:46 -0500

    Initial Commit.
    Right now I am thinking that the structure will be rather simple. The user will have all the options in the “settings.py” file, and the execution will be the “backup.py” script.

---
+++ settings.py
@@ -0,0 +1,12 @@
+'''
+    Settings.py
+    This is where I am going to define the settings for the whole thing.
+
+'''
+
+specifications = {
+        'number_of_backups': 10,
+        'prefix': 'backup_',
+        'source_dir': '',
+        'dest_dir': '',
+    }
